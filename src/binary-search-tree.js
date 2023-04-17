f728aa4f9b3587234c15eb7695fc776c7adbd7ee
siarhei-kliuiko <siarhei.kliuiko@gmail.com>
2023-04-17 10:19:00 +0300

    feat: implement BinarySearchTree class

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,104 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  #root = null;
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+
+    if (this.#root) {
+      const foundNode = this.#tryFindNode(newNode.data, this.#root);
+      if (foundNode.isParent) {
+        if (foundNode.canAddDataToLeft) {
+          foundNode.node.left = newNode;
+        } else {
+          foundNode.node.right = newNode;
+        }
+
+      }
+    } else {
+      this.#root = newNode;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) ? true : false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    const foundNode = this.#tryFindNode(data, this.#root);
+    if (!foundNode.isParent) {
+      return foundNode.node;
+    }
+
+    return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  //Searches for node with specified data. If node not found returns its supposed parent node
+  #tryFindNode(data, parentNode) {
+    let foundNodeData;
+    if (data === parentNode.data) {
+      foundNodeData = { node: parentNode, isParent: false };
+    } else if (data < parentNode.data) {
+      foundNodeData = parentNode.left ? this.#tryFindNode(data, parentNode.left) : { node: parentNode, isParent: true, canAddDataToLeft: true };
+    } else {
+      foundNodeData = parentNode.right ? this.#tryFindNode(data, parentNode.right) : { node: parentNode, isParent: true, canAddDataToLeft: false };
+    }
+
+    return foundNodeData;
+  }
+
+  remove(data) {
+    const removeNode = (data, currentNode) => {
+      let nodeToReturn = currentNode;
+      if (data === currentNode.data) {
+        if (!currentNode.left && !currentNode.right) {
+          nodeToReturn = null;
+        } else if (!currentNode.left) {
+          nodeToReturn = currentNode.right;
+        } else if (!currentNode.right) {
+          nodeToReturn = currentNode.left;
+        } else {
+          const rightMinData = this.#findLeftmostNode(currentNode.right).data;
+          currentNode.data = rightMinData;
+          currentNode.right = removeNode(rightMinData, currentNode.right);
+        }
+      } else if (data < currentNode.data && currentNode.left) {
+        currentNode.left = removeNode(data, currentNode.left);
+      } else if (currentNode.right) {
+        currentNode.right = removeNode(data, currentNode.right);
+      }
+
+      return nodeToReturn;
+    };
+
+    this.#root = removeNode(data, this.#root);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#findLeftmostNode(this.#root).data;
+  }
+
+  #findLeftmostNode(parentNode) {
+    return parentNode.left ? this.#findLeftmostNode(parentNode.left) : parentNode;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const findRightmostNode = parentNode => {
+      return parentNode.right ? findRightmostNode(parentNode.right) : parentNode;
+    }
+
+    return findRightmostNode(this.#root).data;
   }
 }
 
